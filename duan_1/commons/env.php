650588ec73cbc8f415368563f5997ca4ea09df60
rudovu <thovtph52262@gmail.com>
2025-04-07 20:23:56 +0700

    env

--- duan_1/commons/env.php
+++ duan_1/commons/env.php
@@ -3,9 +3,9 @@
 // Biến môi trường, dùng chung toàn hệ thống
 // Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
 
-define('BASE_URL'       , 'http://localhost/duan_mau/');
-define('BASE_URL_ADMIN' , 'http://localhost/duan_mau/admin/');
-define('BASE_URL_CLIENT' , 'http://localhost/duan_mau/client/');
+define('BASE_URL'       , 'http://localhost/da1_nhom6/');
+define('BASE_URL_ADMIN' , 'http://localhost/da1_nhom6/admin/');
+define('BASE_URL_CLIENT' , 'http://localhost/da1_nhom6/client/');
 
 define('DB_HOST'    , 'localhost');
 define('DB_PORT'    , 3306);
