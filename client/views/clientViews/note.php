8b948143b4d6a4ba7c9d420a80b747a88e0b0402
rudovu <thovtph52262@gmail.com>
2025-04-08 16:55:22 +0700

    client

--- client/views/clientViews/note.php
+++ client/views/clientViews/note.php
@@ -7,7 +7,7 @@
             <div class="form-group col-md-6">
                 <ul>
                 <li class="title"><a href="">Ghế tựa</a></li>
-                    <li><i>Không cần giới thiệu nhiều vì chắc mọi người đa số đều đã ăn qua khô gà. Món này siêu hot. Nhưng vị ở nhà Mẹ Cool sẽ đặc biệt hơn. Mùi lá chanh rất thơm, khô gà thấm vị đậm đà nhưng ko hề chát như những nơi khác nhé.</i></li>
+                    <li><i>Ghế tựa bằng da thuộc được đánh giá là nguyên vật liệu có độ bền cao, dẻo dai và lâu bền với thời gian. Nó là thành phẩm được tạo nên từ quy trình thuộc da với nguyên liệu chính là da động vật như trâu, bò, nai, cá sấu, dê, cừu… Trong số đó, da bò được sản xuất với số lượng lớn và phong phú nhất.</i></li>
                 </ul>
             </div>
         </div>
@@ -21,7 +21,7 @@
             <div class="form-group col-md-6">
                 <ul>
                 <li class="title"><a href="">Bộ ghế sofa</a></li>
-                    <li><i>Chân gà sả tắc là một món ăn vặt phổ biến và được yêu thích tại Việt Nam. Với hương vị độc đáo, sự kết hợp giữa chân gà giòn rụm và hương thơm của sả, tắc, món ăn này đã trở thành một phần không thể thiếu trong ẩm thực đường phố.</i></li>
+                    <li><i>Vải nỉ là chất liệu mềm nên được sử dụng rộng rãi để may các đồ nội thất như rèm cửa, thảm trải sàn... Lớp này không chỉ bảo vệ đồ nội thất theo một cách nào đó mà còn giảm độ cứng khi sử dụng áo bọc ghế, giúp sờ vào thoải mái và êm ái hơn, không gây đau nhức lưng do ngồi lâu.</i></li>
                 </ul>
             </div>
         </div>
@@ -36,7 +36,7 @@
             <div class="form-group col-md-6">
                 <ul>
                 <li class="title"><a href="">Bộ Sofa nỉ</a></li>
-                    <li><i>Kimbap hay Gimbap, là món cơm gói trong lá rong biển. Điểm làm nên nét đặc biệt của kimbap chính là phần cơm được trộn cùng dầu mè để tăng vị thơm, kết hợp với rất nhiều nguyên liệu ăn kèm được cuộn vào giữa tạo nên một món vừa ngon lại vừa tiện lợi.</i></li>
+                    <li><i>Đồ nỉ nội thất là một phạm trù để sử dụng để kể về những sản phẩm đồ dùng được sử dụng phục vụ cho cuộc sống gia đình với chất liệu bằng gỗ. Đây là các sản phẩm có lịch sử xuất hiện lâu đời và phát triển qua từng giai đoạn với từng đặc trưng khác nhau, mang đến những trải nghiệm sử dụng mới lạ cho con người.</i></li>
                 </ul>
             </div>
         </div>
@@ -50,7 +50,7 @@
             <div class="form-group col-md-6">
                 <ul>
                 <li class="title"><a href="">Bộ Sofa da</a></li>
-                    <li><i>Với những miếng khoai tây tươi, Ostar đảm bảo sẽ mang lại những miếng bánh giòn, ngọt tự nhiên từ khoai tây, đảm bảo mang lại một hương vị cực kỳ thơm ngon, giúp bạn đánh bay cơn đói ngoài những bữa ăn chính.</i></li>
+                    <li><i>Đồ nội thất bằng da thuộc được đánh giá là nguyên vật liệu có độ bền cao, dẻo dai và lâu bền với thời gian. Nó là thành phẩm được tạo nên từ quy trình thuộc da với nguyên liệu chính là da động vật như trâu, bò, nai, cá sấu, dê, cừu… Trong số đó, da bò được sản xuất với số lượng lớn và phong phú nhất.</i></li>
                 </ul>
             </div>
         </div>
